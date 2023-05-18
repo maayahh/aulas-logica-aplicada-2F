7741cd2ecdac591a90ab8212f5e665ef3dfdf5ae
rm86461 <mayara123santos97@gmail.com>
2023-05-18 08:32:36 -0300

    eu entendi que a classe pessoa e formada por objetos que ajudam a completar e complementar essa classe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Aula de orientaçao a objetos
+        // Declarar objetos
+        Pessoa adao;
+        // instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definiçao do formato
+        adao.nome = "adao";
+        adao.sobrenome = "silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "silva";
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
     }
 }
