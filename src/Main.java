b88b6ad6d0582179194970023c8fc550ccc5eea7
rm86461 <mayara123santos97@gmail.com>
2023-08-08 08:22:23 -0300

    comitando trabalho

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Aula de orientaçao a objetos
-        // Declarar objetos
-        Pessoa adao;
-        // instanciar objetos
-        adao = new Pessoa();
-        Pessoa eva = new Pessoa();
-        // Definiçao do formato
-        adao.nome = "adao";
-        adao.sobrenome = "silva";
-        eva.nome = "Eva";
-        eva.sobrenome = "silva";
-        // Acionar comportamento
-        adao.falar();
-        eva.falar();
 
-        // Criar mais objetos
-        Pessoa qualquer = new Pessoa();
+        // construindo balao();
+        balao b1 = new balao();
 
-        // esclarecer objeto
-        Pessoa rainha;
-                ;
-        // Intanciaçao do objeto
-        rainha = new Pessoa;
+        balao b2 = new balao(10, "azul", "grande", 1.43, 23.65, "grande", 4);
+        balao b3 = new balao(12, "vermelho", "pequeno", 1.21, 78.92, "medio", 12);
 
-        //Definido forma da pessoa
-        rainha.nome = "Tina";
-        rainha.sobrenome = "Tunner";
+        // construindo pessoa
+        pessoa p1 = new pessoa ("Mayara", "7600456", 439023, 55110349, 551190549, "rua. goiania");
+        pessoa p2 = new pessoa ("Raquel", "7600456", 439023, 55110349, 551190549, "rua. limoeira");
 
-        // definiçao de comportamento
-        System.out.println(rainha.falar( "alto"));
-        rainha.comer();
-    }
-}
+        // construindo piloto
+        piloto pi1 = new piloto ("enzo", "7235861", 805205);
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,17 +1,76 @@
-import javax.xml.transform.stream.StreamSource;
+public class Pessoa {
 
-public class Pessoa extends Animal{
-    // Atributos da classe
-    String sobrenome;
-    // Método da classe
-    public void falar (){
-        System.out.println("falei");
+    //Atributos
+    private String nome;
+    private int CPF;
+    private int RG;
+    private float Telefone;
+    private float Telefone_cont;
+    private String Endereco
+    public pessoa(String nome, int CPF, int RG, int telefone, int telefone_cont, String endereço) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.RG = RG;
+        Telefone = telefone;
+        Telefone_cont = telefone_cont;
+        Endereco = endereço;
     }
-    public String falar(String volume){
-        return "falei " +volume;
+
+    public void pessoa(String raquel, String CPF, int rg, float telefone, float telefone_cont, String endereço) {
+
     }
-    // sobrescrita do metodo
-    public void comer(){
-        System.out.println("pessoa comeu");
+
+    public String getNome() {
+        return nome;
     }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCPF(){
+        return getCPF();
+    }
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+    public int getRG() {
+        return RG;
+    }
+
+    public void setRG(int RG) {
+        this.RG = RG;
+    }
+
+    public float getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        Telefone = telefone;
+    }
+
+    public float getTelefone_cont() {
+        return Telefone_cont;
+    }
+
+    public void setTelefone_cont(int telefone_cont) {
+        Telefone_cont = telefone_cont;
+    }
+
+    public String getEndereço() {
+        return Endereco;
+    }
+
+    public void setEndereço(String endereço) {
+        Endereco = endereço;
+    }
+
+    //Métodos
+    public void voa() {System.out.println("Pessoa Viaja"); }
+
+    public void voa(String pessoa_viaja) {
+    }
+
 }
+
